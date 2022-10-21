1d59228e5fdfebd435123e8842dcab97f9437ddb
Ivan Timko <ivan.timko1337@gmail.com>
2022-10-21 18:34:21 +0300

    save1

---
+++ js/lines.js
@@ -0,0 +1,20 @@
+class app {
+    constructor(container) {
+        this.layer = new Layer (container);
+    }
+}
+
+class Layer {
+    constructor(container) {
+        this.canvas = document.createElement(`canvas`); 
+        this.context = this.canvas.getContext(`2d`);
+        container.appendChild(this.canvas);
+
+        this.fitToContainer(this.canvas);
+        addEventListener(`resize`, () => this.fitToContainer(this.canvas));
+    }
+
+fitToContainer(cnv) {
+    cnv.width = cnv.offSetWidth;
+    cnv.height = cnv.offSetHeight;
+}
