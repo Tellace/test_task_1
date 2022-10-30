8f90521a36ff6b899963562e6e1a5021babb11ea
Ivan Timko <ivan.timko1337@gmail.com>
2022-10-30 22:08:42 +0300

    save7

--- js/lines.js
+++ js/lines.js
@@ -1,29 +1,3 @@
-    // class app {
-    //     constructor(container) {
-    //         this.layer = new Layer (container);
-    //     }
-    // }
-
-    // class Layer {
-    //     constructor(container) {
-    //         this.canvas = document.createElement(`canvas`); 
-    //         this.context = this.canvas.getContext(`2d`);
-    //         container.appendChild(this.canvas);
-
-    //         this.fitToContainer(this.canvas);
-    //         addEventListener(`resize`, () => this.fitToContainer(this.canvas));
-    //     }
-
-    // fitToContainer(cnv) {
-    //     cnv.width = cnv.offSetWidth;
-    //     cnv.height = cnv.offSetHeight;
-    //     }
-    // }
-
-    // onload = () => {
-    //     new App(document.querySelector(`field`));
-    // }
-
     var 
     canv = document.getElementById('canvas'),
     ctx = canv.getContext('2d'),
@@ -32,42 +6,10 @@
     canv.width = window.innerWidth;
     canv.height = window.innerHeight;
 
-
-    ctx.lineWidth = 10 * 2;
-
-    // canv.addEvenListener('onclick', function(e) {
-    //     isOnclick = true;
-    //     canv.beginPath();
-    // });
-
-    // canv.addEvenListener('onclick', function() {
-    //     isOnclick = false;
-    //     // ctx.beginPath();
-    //     ctx.closePath();
-    // });
-
-    // // ctx.beginPath();
-    // ctx.moveTo(e.clientX, e.clientY);
-    // ctx.lineTo(e.clientX, e.clientY);
-    // ctx.arc(e.clientX, e.clientY, 10, 0, Math.PI * 2);
-    // ctx.fill();
-    // ctx.stroke();
-    // ctx.closePath();
-
-    
-    canv.addEventListener('mousedown', function() {
-        isOnClick = true;
-    });
-
-    canv.addEventListener('mouseup', function() {
-        isOnClick = Event.stopPropagation;
-        ctx.beginPath();
-    });
-    
     ctx.lineWidth = 5 * 2;
     canv.addEventListener('mousemove', function(e) {
 
-        if( isOnClick) {
+        if( isOnClick ) {
         ctx.lineTo(e.clientX, e.clientY);
         ctx.stroke();
 
@@ -79,22 +21,21 @@
         ctx.moveTo(e.clientX, e.clientY);
         }
     });
+    
+    canv.addEventListener('mousedown', function() {
+        isOnClick = true;
+    });
 
-    // function clearall () {
-    // var canvas = document.getElementById('canvas'),
-    // ctx = canvas.getContext("2d");
-    // ctx.clearRect(0, 0, canvas.width, canvas.height);
-    // };
+    canv.addEventListener('mouseup', function() {
+        isOnClick = Event.stopPropagation;
+        ctx.beginPath();
+    });
 
-    // jQuery('#clearBtn').click(function () {
-    //     ctx.clearRect(0, 0, canvas[0].width, canvas[0].height);
-    // });
+    const clearCanvas = () => {
+        ctx.clearRect(0, 0, canv.width, canv.height);
+    };
 
-    // function clearbtn () {
-    // context.clearRect(0,0,canvas.width,canvas.height);
-    // };
+    const clearbtn = document.querySelector("#clearbtn");
+    clearbtn.addEventListener("click", clearCanvas);
 
-    // var canvas = $( "#canvas" );
-    // $( "#clearbtn" ).on( "click", function( event ) {
-    // canvas.clear();
-    // });
+    
